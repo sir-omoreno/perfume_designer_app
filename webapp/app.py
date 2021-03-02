ab70fcba964a1ee1c626967acf5be6624feb3d93
sir-omoreno <ozmoreno@gmail.com>
2021-03-02 00:16:14 -0500

    testing form code

--- webapp/app.py
+++ webapp/app.py
@@ -6,7 +6,7 @@ import pandas as pd
 import numpy as np
 from flask import Flask, render_template, request, jsonify
 from flask_pymongo import PyMongo
-from pymongo import MongoClient 
+from pymongo import MongoClient
 import joblib
 
 #################################################
@@ -42,10 +42,33 @@ def home():
 def find_your_scent():
     return render_template("find_your_scent.html")
 
+
 @app.route("/perfume_notes")
 def pefume_notes():
-    perfume_notes = list(mongo.db.perfume_notes.find({},{'_id': False}))
+    perfume_notes = list(mongo.db.perfume_notes.find({}, {'_id': False}))
+    print(perfume_notes)
     return jsonify(perfume_notes)
+
+# # testing coding form
+
+
+# @app.route("perfume_form_test", methods=["POST", "GET"])
+# def perfume_form_test():
+#     age = str(request.form['top'])
+
+# # user input list
+#     features = [xxxxx, xxxxx, xxxxx, xxxxx, xxxx, xxxxx, xxxxxxx, xxxxx]
+#     final_features = [np.array(features)]
+#     final_shape = np.reshape(final_features, (1, -1))
+# # load model
+#     model = load("placeholder")
+#     prediction = model.predict(final_shape)
+# #to decide on output page ...
+#     if prediction == 1:
+#         return render_template("placeholder.html")
+#     else:
+#         return render_template("placeholder.html")
+
 
 @app.route("/perfume_popularity/<featuresList>")
 def perfume_popularity(featuresList):
